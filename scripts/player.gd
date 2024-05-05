541ca1dfa1d57c86c3e46a7a2360323a48c297db
JackyXuuu <jacky.x0423@gmail.com>
2024-05-05 18:26:39 -0400

    Character 8-Direction movement
    Figured out the animation of the character movement. Currently working on character dodging and its animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,42 +1,64 @@
 extends CharacterBody2D
 
 
-const SPEED = 50.0
-const JUMP_VELOCITY = -400.0
+const MAXSPEED = 30
+const DODGESPEED = 100
+const ACCELERATION = 100
+const FRICTION = 100
+enum {
+	MOVE,
+	DODGE
+}
+
+var state = MOVE
+var input = Vector2.ZERO
+var dodge_vector = Vector2.DOWN
+var last_direction = input
 
 @onready var animated_sprite = $AnimatedSprite2D
 
+func get_input():
+	input = Input.get_vector("move_left","move_right","move_up","move_down")
+	return input.normalized()
+
 func _physics_process(delta):
-
-	# Get the input direction and handle the movement/deceleration.
-	var isMoving = false
-	# Checking Horizontal movement
-	var direction = Input.get_axis("move_left", "move_right")
-	if direction > 0:
-		animated_sprite.flip_h = false
-	elif direction < 0:
-		animated_sprite.flip_h = true
+	match state:
+		MOVE:
+			move_state(delta)
+		DODGE:
+			dodge_state(delta)
 	
-	if direction:
-		velocity.x = direction * SPEED
-		isMoving = true
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+func move_state(delta):
+	input = get_input()
 	
-	# Checking vertical movement
-	direction = Input.get_axis("move_up", "move_down")
-	if direction:
-		velocity.y = direction * SPEED
-		isMoving = true
+	if input == Vector2.ZERO:
+		var direction_id = int(8.0 * (last_direction.rotated(PI / 8.0).angle() + PI) / TAU)
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		animated_sprite.play(str("idle_", direction_id))
 	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-		
-	# Updating sprite animation
-	if !isMoving:
-		animated_sprite.play("idle-nosword")
-	elif Input.is_action_just_pressed("dodge"):
-		animated_sprite.play("dodge")
+		var direction_id = int(8.0 * (input.rotated(PI / 8.0).angle() + PI) / TAU)
+		last_direction = input
+		velocity = velocity.move_toward(input * MAXSPEED, ACCELERATION * delta)
+		move_and_slide()
+		animated_sprite.play(str("run_", direction_id))
+	print(last_direction)
+	if input != Vector2.ZERO:
+		velocity = velocity.move_toward(input * MAXSPEED, ACCELERATION * delta)
 	else:
-		animated_sprite.play("run")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
+	move()
+	
+	if Input.is_action_just_pressed("dodge"):
+		state = DODGE
+
+func dodge_state(delta):
+	velocity = dodge_vector * DODGESPEED
+	move()
+	animated_sprite.play("dodge")
+	await animated_sprite.animation_finished()
+	state = MOVE
+	
+func move():
 	move_and_slide()
+
